bcb63e627f224c9c51e36a57f50e0a52627e1718
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2023-05-14 15:21:59 +0530

    extended

--- gui.py
+++ gui.py
@@ -1,131 +1,95 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
 from math import log
 from re import compile
 from urllib.parse import urlparse
 from socket import gethostbyname
-from pyquery import PyQuery
-from requests import get
-from json import dump
-from string import ascii_lowercase
-from numpy import array
-import pandas as pd
+
 
 class LexicalURLFeature:
     def __init__(self, url):
-        self.description = 'blah'
         self.url = url
         self.urlparse = urlparse(self.url)
-        self.host = self.__get_ip()
-
-
-    def __get_entropy(self, text):
-        text = text.lower()
-        probs = [text.count(c) / len(text) for c in set(text)]
-        entropy = -sum([p * log(p) / log(2.0) for p in probs])
-        return entropy
-
-    def __get_ip(self):
-        try:
-            ip = self.urlparse.netloc if self.url_host_is_ip() else gethostbyname(self.urlparse.netloc)
-            return ip
-        except:
-            return None
-
-    # extract lexical features
-    def url_scheme(self):
-        return self.urlparse.scheme
 
     def url_length(self):
         return len(self.url)
 
-    def url_path_length(self):
-        return len(self.urlparse.path)
-
-    def url_host_length(self):
-        return len(self.urlparse.netloc)
-
-    def url_host_is_ip(self):
-        host = self.urlparse.netloc
-        pattern = compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
-        match = pattern.match(host)
-        return match is not None
-
-    def url_has_port_in_string(self):
-        has_port = self.urlparse.netloc.split(':')
-        return len(has_port) > 1 and has_port[-1].isdigit()
-
     def number_of_digits(self):
-        digits = [i for i in self.url if i.isdigit()]
-        return len(digits)
+        return len([i for i in self.url if i.isdigit()])
 
     def number_of_parameters(self):
         params = self.urlparse.query
         return 0 if params == '' else len(params.split('&'))
 
-    def number_of_fragments(self):
-        frags = self.urlparse.fragment
-        return len(frags.split('#')) - 1 if frags == '' else 0
+    def url_has_port_in_string(self):
+        has_port = self.urlparse.netloc.split(':')
+        return len(has_port) > 1 and has_port[-1].isdigit()
 
-    def is_encoded(self):
-        return '%' in self.url.lower()
+    def url_path_length(self):
+        return len(self.urlparse.path)
 
-    def num_encoded_char(self):
-        encs = [i for i in self.url if i == '%']
-        return len(encs)
 
-    def url_string_entropy(self):
-        return self.__get_entropy(self.url)
-
-    def number_of_subdirectories(self):
-        d = self.urlparse.path.split('/')
-        return len(d)
-
-    def number_of_periods(self):
-        periods = [i for i in self.url if i == '.']
-        return len(periods)
-
-    def has_client_in_string(self):
-        return 'client' in self.url.lower()
-
-    def has_admin_in_string(self):
-        return 'admin' in self.url.lower()
-
-    def has_server_in_string(self):
-        return 'server' in self.url.lower()
-
-    def has_login_in_string(self):
-        return 'login' in self.url.lower()
-        
-    def get_tld(self):
-      return self.urlparse.netloc.split('.')[-1].split(':')[0]
-    
-#main
-
-pd = pd.read_csv('urldatanew.csv')
-
+pd = pd.read_csv('urldata -Short.csv')
 
 url_length = []
-noofdigits = []
-numofpara = []
-hasport = []
-url_pathlength = []
-
+no_of_digits = []
+num_of_para = []
+has_port = []
+url_path_length = []
 
 for index in pd.index:
     a = LexicalURLFeature(pd['url'][index])
     url_length.append(a.url_length())
-    noofdigits.append(a.number_of_digits())
-    numofpara.append(a.number_of_parameters())
-    hasport.append(a.url_has_port_in_string())
-    url_pathlength.append(a.url_path_length())
-
-
-
+    no_of_digits.append(a.number_of_digits())
+    num_of_para.append(a.number_of_parameters())
+    has_port.append(int(a.url_has_port_in_string()))
+    url_path_length.append(a.url_path_length())
 
 pd['url_length'] = url_length
-pd['no_of_digits'] = noofdigits
-pd['no_of_parameters'] =numofpara
-pd['has_port'] = hasport
-pd['url_path_length'] = url_pathlength
+pd['no_of_digits'] = no_of_digits
+pd['no_of_parameters'] = num_of_para
+pd['has_port'] = has_port
+pd['url_path_length'] = url_path_length
+
+pd.to_csv('features.csv', index=False)
+
+le = LabelEncoder()
+pd['label'] = le.fit_transform(pd['label'])
+
+X = pd.drop(['label', 'url'], axis=1)
+y = pd['label']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, y_train)
 
 
-print(pd)
+def predict_url(url):
+    a = LexicalURLFeature(url)
+    url_length = a.url_length()
+    no_of_digits = a.number_of_digits()
+    num_of_para = a.number_of_parameters()
+    has_port = int(a.url_has_port_in_string())
+    url_path_length = a.url_path_length()
+    label_feature = pd['label'][0]
+    features = [url_length, no_of_digits, num_of_para,
+                has_port, url_path_length, label_feature]
+    features = scaler.transform([features])
+
+    label_pred = knn.predict(features)
+    label_pred = le.inverse_transform(label_pred)
+
+    return label_pred[0]
+
+
+url = input("Enter a URL to predict: ")
+if not url:
+    print(f"The URL {url} is predicted as {predict_url(url)}")
